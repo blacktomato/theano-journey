0a500df5e8e32cd33c0515231fea32cf0cfaacc6
blacktomato <s90169@gmail.com>
2017-04-15 21:57:27 +0800

    on the Mpac station

--- Basic/Algebra/Adding_two_Scalars.py
+++ Basic/Algebra/Adding_two_Scalars.py
@@ -4,10 +4,10 @@
  # File Name : Adding_two_Scalars.py
  # Purpose : Basic scalar operation
  # Creation Date : Sat 15 Apr 2017 08:35:34 PM CST
- # Last Modified : Sat 15 Apr 2017 08:44:07 PM CST
+ # Last Modified : 2017年04月15日 (週六) 21時56分02秒
  # Created By : SL Chung
 ##############################################################
-import numpy
+import numpy as np
 import theano.tensor as T
 from theano import function
 
